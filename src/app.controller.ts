117c742a4a3fe5c2e21fe7d6ec21c4f050160804
Davi Fernandes <davi.fer159@gmail.com>
2026-09-01 13:32:56 -0300

    chore: config module and validation environment variables with zod

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from "@nestjs/common";
+
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
@@ -7,6 +8,7 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    console.log(process.env.POSTGRES_USER);
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import z from "zod";
 
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: z.object({
+        POSTGRES_USER: z.string().nonempty(),
+        POSTGRES_PASSWORD: z.string().nonempty(),
+        POSTGRES_DB: z.string().nonempty(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
